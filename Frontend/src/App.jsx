5a30fc8326a2271d6c66578f62e01529f6ae8e60
Keshav1246 <keshavbansalhsr@gmail.com>
2026-06-05 11:21:06 +0530

    Initialise Project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-green-800 flex items-center justify-center h-screen text-2xl font-bold">
+      Hello, World!
+    </div>
+  )
+}
+
+export default App
